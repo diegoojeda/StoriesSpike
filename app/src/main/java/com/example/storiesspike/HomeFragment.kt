1146f77184badd98d3ac6e2d36062a5474769cca
Diego <diegoojedagarcia@gmail.com>
2021-03-17 10:01:57 +0100

    Input according to design specs

--- app/src/main/java/com/example/storiesspike/HomeFragment.kt
+++ app/src/main/java/com/example/storiesspike/HomeFragment.kt
@@ -3,9 +3,11 @@ package com.example.storiesspike
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.ContextThemeWrapper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
@@ -33,22 +35,24 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
         binding.internalEt.onFocusChangeListener = View.OnFocusChangeListener { _, focusGained ->
             if (focusGained) {
+                binding.textField.hint = "E-MAIL"
                 binding.internalEt.hint = "Type your email here"
-            }
-            else {
+                binding.internalEt.background = ContextCompat.getDrawable(requireContext(), R.drawable.text_field_background_out_focus)
+            } else {
+                binding.textField.hint = "E-mail"
                 binding.internalEt.hint = null
+                binding.internalEt.background = ContextCompat.getDrawable(requireContext(), R.drawable.text_field_background)
             }
         }
 
         binding.internalEt2.onFocusChangeListener = View.OnFocusChangeListener { _, focusGained ->
-
             if (focusGained) {
+                binding.textField2.hint = "PASSWORD"
                 binding.internalEt2.hint = "Type your password here"
-            }
-            else {
+            } else {
+                binding.textField2.hint = "Password"
                 binding.internalEt2.hint = null
             }
-
         }
 
         binding.internalEt.addTextChangedListener(object : TextWatcher {
@@ -59,10 +63,12 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
                 p0?.let {
                     if (it.length > 10) {
                         binding.textField.error = "Mensaje muy largo"
-                    }
-                    else {
+                        binding.internalEt.background = ContextCompat.getDrawable(requireContext(), R.drawable.text_field_background_red)
+                    } else {
                         binding.textField.error = null
+                        binding.internalEt.background = ContextCompat.getDrawable(requireContext(), R.drawable.text_field_background)
                     }
+//                    binding.textField.setErrorTextAppearance(R.style.TextFloatLabelAppearance)
                 }
             }
 
@@ -82,7 +88,7 @@ class StoriesListAdapter(private val items: List<String>, private val onClickLis
     override fun getItemCount(): Int = items.size
 
     class StoriesListVH(private val binding: LibraryNameRowBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(libraryName: String, onClickListener: (String)-> Unit) {
+        fun bind(libraryName: String, onClickListener: (String) -> Unit) {
             binding.libraryNameTv.text = libraryName
             binding.root.setOnClickListener { onClickListener(libraryName) }
         }
